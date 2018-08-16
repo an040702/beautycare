2b648c2df26af7425a3fa87bd9c7525876a5ed83
Doan Thanh Nhan <doanthanhnhan@outlook.com>
2018-08-16 23:45:03 +0700

    Test directive AngularJS --- Check ratingstars.html -- Use FireFox to run

---
+++ js/navbarDirective.js
@@ -0,0 +1,24 @@
+var  myApp = angular.module('myApp',['ngRoute']);
+
+// myApp.config(function ($routeProvider) {
+//     $routeProvider.when('/navbar_01',{
+//         templateUrl: './pages/navbar_01.html',
+//         controller: 'NavbarController'
+//     });
+// });
+
+// Custome directive
+
+myApp.directive('mainNavbar',function () {
+    return {
+        restrict: 'AE',
+        // template: '<h3>Hello AngularJS</h3> <p>I was made inside custom directive</p>',
+        templateUrl: '../pages/navbar_01.html',
+        // replace: false
+    }
+})
+
+// myApp.controller('NavbarController',['$scope',function($scope){
+//     $scope.name = "Home controller";
+//     $scope.detail = "Welcome to home controller";
+// }]);
