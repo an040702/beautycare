563701490fbbd7e4134c760185ec8b1e5750eca9
Doan Thanh Nhan <doanthanhnhan@outlook.com>
2018-08-27 00:09:45 +0700

    Update Kien_CAM...

--- KIEN_CAM_DIT_DKM/js/products.js
+++ KIEN_CAM_DIT_DKM/js/products.js
@@ -4,7 +4,7 @@ var productApp = angular.module("productApp", ["ngRoute"]);
 productApp.config(function($routeProvider) {
     $routeProvider.when('/profile',{
         templateUrl : "hello.html",
-        controller : "productCtrl"
+        controller : "productCtrl_detail"
     });
 });
 
@@ -12,11 +12,30 @@ productApp.controller("productCtrl", function($scope, $http, $routeParams)  {
             $http.get('data/ring_product.json') //reading the product.json file
 
                 .then (function(response){
+                        // console.log(response.data);
+                        // console.log(response.data);
                         $scope.products = response.data; // binding the data to the $scope variable
-                        $scope.product=response.data[$routeParams.name]
+                        // $scope.product = response.data[$routeParams.name]
+                        // console.log($routeParams.name);
                     });
 
-    $scope.id=$scope.product;
+    // $scope.id=$scope.product;
+    }
+);//end controller
+
+productApp.controller("productCtrl_detail", function($scope, $http, $routeParams)  {
+            $http.get('data/ring_product.json') //reading the product.json file
+
+                .then (function(response){
+                        // console.log(response.data);
+                        // console.log(response.data[0].name);
+                        $scope.abc = response.data[0].name; // binding the data to the $scope variable
+                        console.log($scope.abc);
+                        // $scope.product = response.data[$routeParams.name]
+                        // console.log($routeParams.name);
+                    });
+
+    // $scope.TenSP = $scope.abc;
     }
 );//end controller
 
