21ab4062693499e0e51cc779e4829b04fbda1203
karobest3 <karobest3@gmail.com>
2018-08-27 10:16:30 +0700

    s

--- KIEN_CAM_DIT_DKM/js/products.js
+++ KIEN_CAM_DIT_DKM/js/products.js
@@ -2,7 +2,7 @@
 
 var productApp = angular.module("productApp", ["ngRoute"]);
 productApp.config(function($routeProvider) {
-    $routeProvider.when('/profile/:id',{
+    $routeProvider.when('/profile_gold/:id',{
         templateUrl : "profile_gold.html",
         controller : "productCtrl_detail"
     });
@@ -40,13 +40,48 @@ productApp.controller("productCtrl_detail", function($scope, $http, $routeParams
                         $scope.Weight=response.data[$scope.id-1].weight;
                 });
 
-    // $scope.TenSP = $scope.abc;
     }
 );//end controller
-function hide_products(e) {
-    document.getElementById("show_products").style.display="none";
-}
+productApp.config(function($routeProvider) {
+    $routeProvider.when('/profile_diamon/:id',{
+        templateUrl : "profile_gold.html",
+        controller : "productCtrl_detail_diamon"
+    });
+});
+productApp.controller("productCtrl_diamon", function($scope, $http, $routeParams)  {
+        $http.get('data/ring_diamon_product.json') //reading the product.json file
 
+            .then (function(response){
+                $scope.products = response.data; // binding the data to the $scope variable
+
+            });
+    }
+);//end controller
+
+productApp.controller("productCtrl_detail_diamon", function($scope, $http, $routeParams)  {
+        $http.get('data/ring_diamon_product.json') //reading the product.json file
+            .then (function(response){
+                $scope.id=$routeParams.id;
+                $scope.image=response.data[$scope.id-1].image;
+                $scope.name_product=response.data[$scope.id-1].name;
+                $scope.sex=response.data[$scope.id-1].sex;
+                $scope.price=response.data[$scope.id-1].price;
+                $scope.price_8=response.data[$scope.id-1].price_8;
+                $scope.price_9=response.data[$scope.id-1].price_9;
+                $scope.price_10_5=response.data[$scope.id-1].price_10_5;
+                $scope.title_info=response.data[$scope.id-1].title_info;
+                $scope.description=response.data[$scope.id-1].description;
+                $scope.Trade_Mark=response.data[$scope.id-1].trade_mark;
+                $scope.Stone=response.data[$scope.id-1].stone;
+                $scope.Color=response.data[$scope.id-1].color;
+                $scope.Shape=response.data[$scope.id-1].shape;
+                $scope.Cara=response.data[$scope.id-1].cara;
+                $scope.Age=response.data[$scope.id-1].age;
+                $scope.Weight=response.data[$scope.id-1].weight;
+            });
+
+    }
+);//
 $(document).ready(function(){
     $(window).scroll(function(){
         if ($(this).scrollTop() > 100) {
