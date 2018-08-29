4a7a27c12efab9d84bd34d5693e5d1790b10a3b2
Doan Thanh Nhan <doanthanhnhan@outlook.com>
2018-08-29 16:10:08 +0700

    Fix navbar index.html

--- js/products.js
+++ js/products.js
@@ -1,21 +1,4 @@
 //creating an application module
-var  myApp = angular.module('myApp',['ngRoute']);
-
-// Custome directive
-
-myApp.directive('mainNavbar',function () {
-    return {
-        restrict: 'AE',
-        templateUrl: '../pages/navbar_01.html',
-    }
-});
-
-myApp.directive('mainNavbarHomePage',function () {
-    return {
-        restrict: 'AE',
-        templateUrl: './common/navbar.html',
-    }
-});
 
 //Products App module
 
