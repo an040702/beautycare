b00446cc2f767df7fc21f1ff40c7c070487b43a6
karobest3 <karobest3@gmail.com>
2018-09-06 17:04:58 +0700

    s

--- js/products.js
+++ js/products.js
@@ -37,71 +37,167 @@ productApp.config(['$routeProvider','$locationProvider',function($routeProvider,
     }).when('/profile/:name/:id',{
         templateUrl : "pages/profile_ring.html",
         controller : "productCtrl"
-    }).otherwise({redirectTo:'/home'});;
+    }).when('/checkout',{
+        templateUrl : "pages/check_out.html"
+    }).otherwise({redirectTo:'/home'});
     // $locationProvider.html5Mode(true);
 }]);
-var duongDan = window.location.pathname;
-productApp.controller("productCtrl", function($scope, $http,$routeParams,$location)  {
-            $scope.currentPage = 1;
-            $scope.pageSize = 12;
-            $scope.name_custom=$routeParams.name;
-            console.log(duongDan);
-            if(duongDan != ""){
-                // duongDan="";
-                // $location.path("/index.html");
 
+productApp.controller("productCtrl", function($scope, $http,$routeParams,$location) {
+    $scope.currentPage = 1;
+    $scope.pageSize = 12;
+    $scope.name_custom = $routeParams.name;
+    $http.get('data/ring_' + $routeParams.name + '.json') //reading the product.json file
+        .then(function (response) {
+            $scope.products = response.data; // binding the data to the $scope variable
+            $scope.id = $routeParams.id;
+            $scope.image = response.data[$scope.id - 1].image;
+            $scope.name_product = response.data[$scope.id - 1].name;
+            $scope.sex = response.data[$scope.id - 1].sex;
+            $scope.price = response.data[$scope.id - 1].price;
+            $scope.price_8 = response.data[$scope.id - 1].price_8;
+            $scope.price_9 = response.data[$scope.id - 1].price_9;
+            $scope.price_10_5 = response.data[$scope.id - 1].price_10_5;
+            $scope.title_info = response.data[$scope.id - 1].title_info;
+            $scope.description = response.data[$scope.id - 1].description;
+            $scope.Trade_Mark = response.data[$scope.id - 1].trade_mark;
+            $scope.Stone = response.data[$scope.id - 1].stone;
+            $scope.Color = response.data[$scope.id - 1].color;
+            $scope.Shape = response.data[$scope.id - 1].shape;
+            $scope.Cara = response.data[$scope.id - 1].cara;
+            $scope.Age = response.data[$scope.id - 1].age;
+            $scope.Weight = response.data[$scope.id - 1].weight;
+        });
+
+    //SHOPPING...........................................................
+
+            var array = [];
+            var name_product;
+            var price_product;
+            var product_type;
+            var image_product;
+            var no;
+            var k=0;
+            var check=[];
+            var id;
+            $scope.add_Cart = function (checked_id) {
+                var check_tam=0;
+
+                if(document.getElementById("btn_add_"+ checked_id).style.display=="block") {
+                    document.getElementById("btn_add_" + checked_id).style.display = "none";
+                    document.getElementById("btn_added_" + checked_id).style.display = "block";
+                }
+                else{
+                    document.getElementById("btn_add_" + checked_id).style.display = "block";
+                    document.getElementById("btn_added_" + checked_id).style.display = "none";
+                }
+                var check_id=true;
+                var tam=0;
+                check[k] = document.getElementById("id_sp_" + checked_id).innerText;
+                name_product = document.getElementById("name_" + checked_id).innerText;
+                price_product = document.getElementById("price_" + checked_id).innerText;
+                product_type = $routeParams.name;
+                id = document.getElementById("id_sp_" + checked_id).innerText;
+                image_product = "images/images_ring/images_ring_" + $routeParams.name + "/" + $routeParams.name + "_" + checked_id + ".png";
+                no=array.length+1;
+                if (k == 0) {
+                    array.push({no : no, image: image_product,id:id, type: product_type, name: name_product, price: price_product});
+                    alert("Bạn đã chọn " + array.length + " sản phẩm");
+                    k++;
+                }
+                else {
+                    for (tam; tam < check.length-1; tam++) {
+                        if (check[k] == check[tam]) {
+                            check_tam=tam;
+                            check_id=false;
+                            break;
+
+                        }
+                        else {
+                            check_id=true;
+                        }
+                    }
+                    if(check_id==true){
+                        array.push({
+                            no : no,
+                            image: image_product,
+                            id : id,
+                            type: product_type,
+                            name: name_product,
+                            price: price_product
+                        });
+                        alert("Bạn đã chọn " + array.length + " sản phẩm");
+                        k++;
+                    }
+                    else {
+                        var change_array_1=check_tam;
+                        alert("Bạn đã chọn sản phẩm này rồi !!!");
+                        for(change_array_1;change_array_1<k;change_array_1++){
+                            array[change_array_1]=array[change_array_1+1];
+                        }
+                        k--;
+                        alert(k);
+                    }
+                }
+                no=array.length+1;
             }
-            $http.get('data/ring_'+$routeParams.name+'.json') //reading the product.json file
-                .then(function (response) {
-                    $scope.products = response.data; // binding the data to the $scope variable
-                    // $scope.id=$routeParams.id;
-                    // $scope.image = response.data[$scope.id-1].image;
-                    // $scope.name_product=response.data[$scope.id-1].name;
-                    // $scope.sex=response.data[$scope.id-1].sex;
-                    // $scope.price=response.data[$scope.id-1].price;
-                    // $scope.price_8=response.data[$scope.id-1].price_8;
-                    // $scope.price_9=response.data[$scope.id-1].price_9;
-                    // $scope.price_10_5=response.data[$scope.id-1].price_10_5;
-                    // $scope.title_info=response.data[$scope.id-1].title_info;
-                    // $scope.description=response.data[$scope.id-1].description;
-                    // $scope.Trade_Mark=response.data[$scope.id-1].trade_mark;
-                    // $scope.Stone=response.data[$scope.id-1].stone;
-                    // $scope.Color=response.data[$scope.id-1].color;
-                    // $scope.Shape=response.data[$scope.id-1].shape;
-                    // $scope.Cara=response.data[$scope.id-1].cara;
-                    // $scope.Age=response.data[$scope.id-1].age;
-                    // $scope.Weight=response.data[$scope.id-1].weight;
-                });
-    }
+           function displayTable(table_shopping) {
+                document.getElementById('show_table').innerHTML="<table id='table_shopping'><tr><th>No</th><th>Image</th><th>Name</th><th>Id</th><th>Type</th><th>Price</th></tr></table>";
+                var table = document.getElementById('table_shopping');
+                var sum=0;
+                for (var i = 0; i < table_shopping.length; ++i)
+                {   // keep a reference to an individual president object
+                    var products = table_shopping[i];
+
+                    // create a row element to append cells to
+                    var row = document.createElement('tr');
+
+                    // properties of the array elements
+                    var properties = ['no','image','name','type','id', 'price'];
+
+                    // append each one of them to the row in question, in order
+                    for (var j = 0; j < properties.length; ++j) {   // create new data cell for names
+                        if (j == 1) {
+                            var cell = document.createElement('img');
+                            cell.setAttribute('src',products[properties[j]]);
+                            cell.setAttribute('height','50px');
+                            cell.setAttribute('style','margin:10px');
+                        }
+                        else {
+                            var cell = document.createElement('td');
+                            cell.innerHTML = products[properties[j]];
+                            if (j == properties.length-1) {
+                                sum+= parseFloat(products[properties[j]]);
+                            }
+                        }
+                        // set name of property using bracket notation (properties[i] is a string,
+                        // which can be used to access properties of president)
+
+                        // add to end of the row
+                        row.appendChild(cell);
+                    }
+                    // add new row to table
+                    table.appendChild(row);
+                    if(i==table_shopping.length-1){
+                        var row = document.createElement('tr');
+                        var cell=document.createElement('td');
+                        cell.setAttribute('colspan',properties.length-1);
+                        cell.setAttribute('style','color:blue');
+                        cell.innerHTML="TOTAL";
+                        row.appendChild(cell);
+                        var cell=document.createElement('td');
+                        cell.setAttribute('style','color:blue');
+                        cell.innerHTML=sum;
+                        row.appendChild(cell);
+                        table.appendChild(row);
+                    }
+                }
+            }
+            $scope.show_shopping=function(){
+                displayTable(array);
+            }
+        }
 );//end controller
-
-//Product detail page controller
-// productApp.controller("productCtrl_detail", function($scope, $http, $routeParams)  {
-//             $http.get('../data/ring_product.json') //reading the product.json file
-//                 .then (function(response){
-//                         $scope.id=$routeParams.id;
-//                         console.log($routeParams.id);
-//                         $scope.image=response.data[$scope.id-1].image;
-//                         $scope.name_product=response.data[$scope.id-1].name;
-//                         $scope.sex=response.data[$scope.id-1].sex;
-//                         $scope.price=response.data[$scope.id-1].price;
-//                         $scope.price_8=response.data[$scope.id-1].price_8;
-//                         $scope.price_9=response.data[$scope.id-1].price_9;
-//                         $scope.price_10_5=response.data[$scope.id-1].price_10_5;
-//                         $scope.title_info=response.data[$scope.id-1].title_info;
-//                         $scope.description=response.data[$scope.id-1].description;
-//                         $scope.Trade_Mark=response.data[$scope.id-1].trade_mark;
-//                         $scope.Stone=response.data[$scope.id-1].stone;
-//                         $scope.Color=response.data[$scope.id-1].color;
-//                         $scope.Shape=response.data[$scope.id-1].shape;
-//                         $scope.Cara=response.data[$scope.id-1].cara;
-//                         $scope.Age=response.data[$scope.id-1].age;
-//                         $scope.Weight=response.data[$scope.id-1].weight;
-//                 });
-//
-//     }
-// );//end controller
-
 
 $(document).ready(function(){
     $(window).scroll(function(){
@@ -116,27 +212,3 @@ $(document).ready(function(){
         return false;
     });
 });
-// $(window).on('beforeunload',function(){
-
-//      // location.pathname.replace('/#!'+location.pathname)
-//      this.location.assign("#!/" + location.pathname);
-//     // return 'are you sure you want to leave?';
-
-// });
-// $( window ).unload(function() {
-//   return "Bye now!";
-// });
-
-// function reload(){
-//     window.location.assign("#!"+window.location.pathname);
-// };
- // window.onbeforeunload = function(event)
- //    {
- //        window.location.assign("#!"+window.location.pathname);
- //        return confirm("Confirm refresh");
- //    };
-
-// $(window).bind("beforeunload", function(){
-//     window.location.assign("#!"+window.location.pathname);
-//         return confirm("Do you really want to refresh?"); 
-// });
