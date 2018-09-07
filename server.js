87c9fad21af5c65a46871f5e677a54b0a27dff83
Doan Thanh Nhan <doanthanhnhan@outlook.com>
2018-09-07 10:01:49 +0700

    Fix parallax effects...

--- server.js
+++ server.js
@@ -8,85 +8,44 @@ var express = require('express');
 var app = express();
 var serveStatic = require('serve-static');
 const url = require('url');
+var callback = function() {} ;
 
+var obj = {
+   table: []
+};
 
+obj.table.push({id: 1, square:2});
+
+var json = JSON.stringify(obj);
+
+var fs = require('fs');
+
+fs.writeFile('test/user.json', json, 'utf8', callback);
+
+fs.readFile('test/user.json', 'utf8', function readFileCallback(err, data){
+    if (err){
+        console.log(err);
+    } 
+    else 
+    {
+		    obj = JSON.parse(data); //now it an object
+		    console.log(obj+"jsjssjs");
+		    var dataadd = new Array({id: 2, square:4});
+		    console.log(dataadd+"bbbbb");
+
+		    obj.table.push(dataadd); //add some data
+		    json = JSON.stringify(obj); //convert it back to json
+		    console.log(json+"json");
+		    fs.writeFile('test/user.json', json, 'utf8', callback); // write it back 
+	}
+});
 /**
  * STATIC FILES
  */
 app.use(express.static(__dirname+"/"));
 
-
-// Default every route except the above to serve the index.html
-// app.get('*', function(req, res) {  
-//     res.sendFile(path.join(__dirname));
-// });
-// app.get('/', function (req, res) {
-//    res.send('Hello World');
-// })
-// app.use('/*',function(req, res) {
-//     res.sendfile(__dirname + '/pages/index.html');
-// });
-// This is a server file
-// app.get("*", function(req, res) {
-//     res.render("./index.html");
-// });
-// app.get('/beautycare/*', function (req, res) {
-// res.sendFile(path.join(__dirname, './pages', 'index.html')); // change this
-// });
-// app.get('/', function(req, res) {
-//     res.sendFile( __dirname + "/" + "index.html" );
-// });
 app.listen(8080, function(){
 	console.log(__dirname);
     console.log('Server running on 8080...');
     // console.log(__dirname);
 });
-// app.use(express.static('pages'));
-// app.get('pages/index.html', function (req, res) {
-//    res.sendFile( __dirname + "/" + "pages/index.html" );
-// })
-
-// var server = app.listen(8081, function () {
-//   var host = server.address().address
-//   var port = server.address().port   
-//   console.log("Example app listening at http://%s:%s", host, port)
-// })
-
-// var express = require('express');
-// var path = require('path');
-// var router = express.Router();
- 
-// // serve angular front end files from root path
-// router.use('/', express.static('app', { redirect: false }));
- 
-// // rewrite virtual urls to angular app to enable refreshing of internal pages
-// router.get('*', function (req, res, next) {
-//     res.sendFile(path.resolve('pages/index.html'));
-// });
- 
-// module.exports = router;
-
-// var http = require('http');
-// var fs = require('fs');
-
-// const PORT=8080; 
-
-// fs.readFile('./index.html', function (err, html) {
-
-//     if (err) throw err;    
-
-//     http.createServer(function(request, response) {  
-//         response.writeHeader(200, {"Content-Type": "text/html"});  
-//         response.write(html);  
-//         response.end();  
-//     }).listen(PORT);
-// });
-
-// var express = require('express');
-// var app = express();
-
-// console.log(__dirname);
-// app.use(express.static(__dirname)); //__dir and not _dir
-// var port = 8080; // you can use any port
-// app.listen(port);
-// console.log('server on' + port);
