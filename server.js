c9c5771cae02fce13db330bcd1e0767697a2771c
Doan Thanh Nhan <doanthanhnhan@outlook.com>
2018-09-11 00:07:24 +0700

    Add images...

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
-// var connect = require('connect');
-// var serveStatic = require('serve-static');
-// connect().use(serveStatic(__dirname)).listen(8080, function(){
-//     console.log('Server running on 8080...');
-// });
-
 var express = require('express');
 var app = express();
 var serveStatic = require('serve-static');
 const url = require('url');
 var callback = function() {} ;
 var fs = require('fs');
-
-
 var obj;
 var json = JSON.stringify(obj);
 
@@ -21,18 +13,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
  });
+
 //Post data_test from test.html
 app.post('/', function(req, res){
-  console.log(req.body);
   res.send("ok. NodeJS response");
   obj = req.body;
   json = JSON.stringify(obj); //convert it back to json
-	console.log(json+" json");
 	fs.writeFile('test/user1.json', json, 'utf8', callback); // write it back 
 	
 });
@@ -41,27 +33,20 @@ app.post('/', function(req, res){
 app.post('/product/:name', function(req, res){
   //Get :name from product page
   var name = req.params.name;
-  console.log(req.body);
-  console.log(req.params.name);
   res.send("Got data_array. NodeJS response");
   obj = req.body;
   json = JSON.stringify(obj); //convert it back to json
-  // console.log(json+" json");
-  fs.writeFile('data/user.json', json, 'utf8', callback); // write it back 
- 
-});
-//Post data_array from diamon product page
-app.post('/checkout', function(req, res){
-  console.log(req.body);
-  res.send("Got data_array. NodeJS response");
-  obj = req.body;
-  json = JSON.stringify(obj); //convert it back to json
-  // console.log(json+" json");
   fs.writeFile('data/user.json', json, 'utf8', callback); // write it back 
   console.log("Saved!!!");
 });
-
-
+//Post data_array from diamon product page
+app.post('/checkout', function(req, res){
+  res.send("Got data_array. NodeJS response");
+  obj = req.body;
+  json = JSON.stringify(obj); //convert it back to json
+  fs.writeFile('data/user.json', json, 'utf8', callback); // write it back 
+  console.log("Saved!!!");
+});
 
 /**
  * STATIC FILES
@@ -69,6 +54,5 @@ app.post('/checkout', function(req, res){
 app.use(express.static(__dirname+"/"));
 
 app.listen(8080, function(){
-	console.log(__dirname);
   console.log('Server running on 8080...');
 });
