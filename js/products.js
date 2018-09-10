7d3b38dc0f986545c664c15f8d35ad596f527040
vovanduc <voduc204@gmail.com>
2018-09-10 16:31:22 +0700

    Thay doi duong dan den file json best sell

--- js/products.js
+++ js/products.js
@@ -75,27 +75,27 @@ productApp.config(['$routeProvider','$locationProvider',function($routeProvider,
 
 // BEST SELLER
 productApp.controller("myCtrl",function($scope,$http) {
-    $http.get('./data/BESTSELLER/sell_hair.json')
+    $http.get('./data/best_sell/sell_hair.json')
         .then(function(response) {
         $scope.hairhome = response.data;
     });
-    $http.get('./data/BESTSELLER/sell_skin.json')
+    $http.get('./data/best_sell/sell_skin.json')
         .then(function(response) {
         $scope.skinshome = response.data;
     });    
-    $http.get('./data/BESTSELLER/sell_makeup.json')
+    $http.get('./data/best_sell/sell_makeup.json')
         .then(function(response) {
         $scope.makeuphome = response.data;
     });
-    $http.get('./data/BESTSELLER/sell_nails.json')
+    $http.get('./data/best_sell/sell_nails.json')
         .then(function(response) {
         $scope.nailshome = response.data;
     });
-    $http.get('./data/BESTSELLER/sell_jewellry.json')
+    $http.get('./data/best_sell/sell_jewellry.json')
         .then(function(response) {
         $scope.jewellryhome = response.data;
     });
-    $http.get('./data/BESTSELLER/sell_wedding.json')
+    $http.get('./data/best_sell/sell_wedding.json')
         .then(function(response) {
         $scope.weddinghome = response.data;
     });
